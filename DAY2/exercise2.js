57e1fa2e23867a6f183ab535e54b909f486ed431
reffn <reffanagustian02@gmail.com>
2022-11-10 09:40:29 +0700

    exercise2_day2

---
+++ DAY2/exercise2.js
@@ -0,0 +1,21 @@
+//write a code to convert celcius to fahrenheit
+let celcius = 20;
+let fahrenheit = celcius * (9/5) + 32;
+
+console.log("Celcius to fahrenheit is = " + fahrenheit);
+
+//write a code to check odd or even number
+let number = 2;
+
+if(number % 2 == 0){
+    console.log("This is even number");
+}
+
+else{
+    console.log("This is odd number");
+}
+
+//write a code to check prime number or not
+
+let primeNumber = 5;
+
