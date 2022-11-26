be8dacbf1138bdf5745f512c515c9c91f51c8b71
reffn <reffanagustian02@gmail.com>
2022-11-26 07:35:12 +0700

    add exercise12

---
+++ DAY12/exercise12.js
@@ -0,0 +1,5 @@
+function tambah(nilai1, nilai2) {
+  return nilai1 + nilai2;
+}
+console.log(tambah(2, 5));
+console.log(tambah(3, 3));
--- DAY2/exercise2.js
+++ DAY2/exercise2.js
@@ -1,111 +1,97 @@
 //write a code to convert celcius to fahrenheit
-//easy 
 
 let celcius = 20;
-let fahrenheit = celcius * (9/5) + 32;
+let fahrenheit = celcius * (9 / 5) + 32;
 
 console.log("Celcius to fahrenheit is = " + fahrenheit);
 
 //write a code to check odd or even number
-//intermediate
 
-let number = 4;
+let number = 1;
 
-if(number % 2 == 0){
-    console.log("This is even number");
-}
-
-else{
-    console.log("This is odd number");
+if (number % 2 == 0) {
+  console.log("This is even number");
+} else {
+  console.log("This is odd number");
 }
 
 //write a code to check prime number or not
-// verry hard must try again
 
 let primeNumber = true;
-if(number == 0 || number == 1)
+if (number == 0 || number == 1) primeNumber = false;
+
+for (let i = 2; i <= number / 2; ++i) {
+  if (number % i == 0) {
     primeNumber = false;
-
-for(let i = 2 ; i <= number / 2 ; ++i){
-    if(number % i == 0){
-        primeNumber = false;
-    }
+  }
 }
+primeNumber
+  ? console.log(number + " is a prime number. ")
+  : console.log(number + " is not a prime number. ");
 
-primeNumber ? console.log(number + " is a prime number. ") :
-                console.log(number + " is not a prime number. ");
-
-
-//find the sum of the number 
-//hard must try again
+//find the sum of the number
 
 let sum = 0;
-let text = `${number} -> `
-for(let i = 1 ; i <= number ; i++){
-    i == 1 ? text += `${i}` : text += ` +${i}` 
-    sum += i;
+let text = `${number} -> `;
+for (let i = 1; i <= number; i++) {
+  i == 1 ? (text += `${i}`) : (text += ` +${i}`);
+  sum += i;
 }
 
 console.log(text + " = " + sum);
 
-//find factorical number 
-//hard must try again
+//find factorical number
 
 let factorical = 1;
 text = `${number} ->`;
-    for(let i = 1 ; i <= number ; i++){
-        i == 1 ? text += ` ${i}` : text += ` x ${i}`
-        factorical = factorical * i;
+for (let i = 1; i <= number; i++) {
+  i == 1 ? (text += ` ${i}`) : (text += ` x ${i}`);
+  factorical = factorical * i;
 }
 console.log(`Factorical of ${text} = ${factorical}`);
 
 //write a code to print first N fibonacci number
-//verry hard must try again
 
-let temp1 = 0 , temp2 = 1 , temp3 = 0;
+let temp1 = 0,
+  temp2 = 1,
+  temp3 = 0;
 number = 10;
 text = `${number} number(s) of fibonacci : ${temp1} , ${temp2}`;
-for(let i = 2 ; i < number ; ++i){
-    temp3 = temp1 + temp2;
-    text += `${temp3}`
-    temp1 = temp2;
-    temp2 = temp3;
+for (let i = 2; i < number; ++i) {
+  temp3 = temp1 + temp2;
+  text += `${temp3}`;
+  temp1 = temp2;
+  temp2 = temp3;
 }
 console.log(text);
 
-//bmi 
-//intermediate
+//bmi
 
 let height = 170;
 let weight = 65;
-let bmi = weight / ((height / 100) ** 2);
+let bmi = weight / (height / 100) ** 2;
 console.log(bmi);
 
-if(bmi > 18.5 && bmi < 24.9){
-    console.log("ideal");
-}
-else{
-    console.log("tidak ideal");
+if (bmi > 18.5 && bmi < 24.9) {
+  console.log("ideal");
+} else {
+  console.log("tidak ideal");
 }
 
 //fuzz buzz challange
-//verry hard must try again
 
-text = ""
+text = "";
 number = 15;
-for(let i = 1 ; i <= number ; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-        text += ` FuzzBuzz `
-    }
-    else if(i % 5 == 0){
-        text += ` Fuzz `
-    }
-    else if(i % 3 == 0){
-        text += ` Buzz `
-    }
-    else{
-        text += `${i}`
-        console.log(i);
-    }
+for (let i = 1; i <= number; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    text += ` FuzzBuzz `;
+  } else if (i % 5 == 0) {
+    text += ` Fuzz `;
+  } else if (i % 3 == 0) {
+    text += ` Buzz `;
+  } else {
+    text += `${i}`;
+    console.log(i);
+  }
 }
 console.log(text);
